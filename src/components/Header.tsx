13b6e013569e5dc570bdda30be5f331a9526eb01
JeanC4rlo <jeancs.santos1@gmail.com>
2026-08-08 16:44:03 -0300

    feat(ui): add layout elements and resizable Separator component
    Add brand image assets, Header and Logo components, and a mouse-draggable Separator component for split view dynamic resizing.

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./Logo"
+
+export const Header = () => {
+    return (
+        <header className="flex justify-center h-16 py-2 shadow-sm">
+            <Logo />
+        </header>
+    )
+}
